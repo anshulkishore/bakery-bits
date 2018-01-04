4ec3e43d1b6037b80a67195cb5cf1f57249de0a8
Anshul Kishore <anshulkishore310@gmail.com>
2018-01-04 19:30:46 +0530

    Added all files

---
+++ PROJECTSQL/cupcake.sql
@@ -0,0 +1,15 @@
+create table cupcake (CID varchar(5) primary key, FLAVOURS VARCHAR(40), PRICE_PER_PIECE INT );
+
+INSERT INTO cupcake VALUES("cp1","Vanilla Bean",40);
+
+INSERT INTO cupcake  VALUES("cp2","Black n white",40);
+
+INSERT INTO cupcake  VALUES("cp3","Fudge Brownie Cupcake",45);
+
+INSERT INTO cupcake VALUES("cp4","Bitter Sweet Dark Choco",50);
+
+INSERT INTO cupcake  VALUES("cp5","Choco Peanut Butter Cup",35);
+
+INSERT INTO cupcake VALUES("cp6","Mijer Lemon Rasberry",35);
+
+INSERT INTO cupcake  VALUES("cp7","Old Fashioned Cupcake",40);
